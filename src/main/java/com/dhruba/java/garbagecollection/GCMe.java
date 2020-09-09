74ad04a3e5a12084f03c603e2e7aabefe67add91
Dhruba Nag <dhrub123@gmail.com>
2020-09-09 19:32:49 +0530

    Master : GC 2

---
+++ src/main/java/com/dhruba/java/garbagecollection/GCMe.java
@@ -0,0 +1,34 @@
+package com.dhruba.java.garbagecollection;
+
+public class GCMe {
+	long data;
+	long a;
+	long aa;
+	long aaa;
+	long aaaa;
+	long aaaaa;
+	long aaaaaa;
+	long aaaaaaa;
+	long aaaaaaaa;
+	long aaaaaaaaa;
+	long aaaaaaaaaa;
+	long aaaaaaaaaaa;
+	long aaaaaaaaaaaa;
+	long aaaaaaaaaaaaa;
+	long aaaaaaaaaaaaaa;
+	long aaaaaaaaaaaaaaa;
+	long aaaaaaaaaaaaaaaa;
+	long aaaaaaaaaaaaaaaaa;
+	long aaaaaaaaaaaaaaaaaa;
+	long aaaaaaaaaaaaaaaaaaa;
+	long aaaaaaaaaaaaaaaaaaaa;
+	long aaaaaaaaaaaaaaaaaaaaa;
+	long aaaaaaaaaaaaaaaaaaaaaaa;
+	long aaaaaaaaaaaaaaaaaaaaaaaa;
+	long aaaaaaaaaaaaaaaaaaaaaaaaa;
+	long aaaaaaaaaaaaaaaaaaaaaaaaaa;
+	long aaaaaaaaaaaaaaaaaaaaaaaaaaa;
+	long aaaaaaaaaaaaaaaaaaaaaaaaaaaa;
+	long aaaaaaaaaaaaaaaaaaaaaaaaaaaaa;
+	long aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa;
+}
